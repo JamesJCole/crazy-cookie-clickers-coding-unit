fa4679d303443fd0e4cf1ff3cc8d3cff6a00d9ac
James Cole <JamesJCole@users.noreply.github.com>
2026-06-26 18:28:23 +1000

    Adding assets for updates to the skillmap

---
+++ assets/controls/encodedValues.js
@@ -0,0 +1,24 @@
+let BakeLabel = sprites.create(img`
+  ..fffffffffffffffffffffff..
+  .f11111111111111111111111f.
+  f11fff111ff11f11f1fff11f11f
+  f11f11f1f11f1f1f11f1111f11f
+  f11fff11ffff1ff111fff11f11f
+  f11f11f1f11f1f1f11f1111111f
+  f11ffff1f11f1f11f1fff11f11f
+  .f11111111111111111111111f.
+  ..fffffffffffffffffffffff..
+`, SpriteKind.Player)
+
+
+let UpgradeLabel = sprites.create(img`
+  ..fffffffffffffffffffffffffffffffffffff..
+  .f1111111111111111111111111111111111111f.
+  f11f11f1ffff1fff11ffff11ff11fff11fff1f11f
+  f11f11f1f11f1f1111f11f1f11f1f11f1f111f11f
+  f11f11f1ffff1f1ff1ffff1ffff1f11f1fff1f11f
+  f11f11f1f1111f11f1f1f11f11f1f11f1f111111f
+  f11ffff1f1111ffff1f11f1f11f1fff11fff1f11f
+  .f1111111111111111111111111111111111111f.
+  ..fffffffffffffffffffffffffffffffffffff..
+`, SpriteKind.Player)
